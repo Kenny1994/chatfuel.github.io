7eca0381de512fd949f25d8ad5c75ec470d8cc6d
kenny <hoangnd@mageplaza.com>
2018-08-20 10:51:50 +0700

    dsa

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+date_default_timezone_set("America/New_York");
+$time = date("H:i:s");
+sendText("Bây giờ là ". $time.  "Ở Mỹ");
+function sendText($text){
+    $arr = array (
+        'messages' =>
+            array (
+                0 =>
+                    array (
+                        'text' => $text,
+                    ),
+            ),
+    );
+    echo json_encode($arr);
+
+}
